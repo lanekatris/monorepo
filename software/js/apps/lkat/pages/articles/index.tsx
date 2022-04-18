23e123f733c0a8d57bb2a21be2737a4da35253bf
Lane Katris <lanekatris@gmail.com>
2022-04-18 16:16:26 -0400

    added sorting and testing api on vercel

--- software/js/apps/lkat/pages/articles/index.tsx
+++ software/js/apps/lkat/pages/articles/index.tsx
@@ -5,6 +5,10 @@ import matter from 'gray-matter';
 import Link from 'next/link';
 import Image from 'next/image';
 
+function reverseSort(a, b) {
+  return -1 * a.slug.localeCompare(b.slug);
+}
+
 export const getStaticProps = async () => {
   const files = fs.readdirSync(path.join(process.cwd(), 'posts'));
   const posts = files.map((filename) => {
@@ -18,6 +22,7 @@ export const getStaticProps = async () => {
       slug: filename.split('.')[0],
     };
   });
+  posts.sort(reverseSort);
   return {
     props: {
       posts,
@@ -32,6 +37,60 @@ export function Articles({ posts }: ArticlesProps) {
   console.log('posts', posts);
   return (
     <div className="mt-5 pb-5">
+      <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        {/*<img*/}
+        {/*  className="object-cover w-full h-56"*/}
+        {/*  src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"*/}
+        {/*  alt="avatar"*/}
+        {/*/>*/}
+
+        {/*<div className="py-5 text-center">*/}
+        {/*  <a*/}
+        {/*    href="#"*/}
+        {/*    className="block text-2xl font-bold text-gray-800 dark:text-white"*/}
+        {/*  >*/}
+        {/*    John Doe*/}
+        {/*  </a>*/}
+        {/*  <span className="text-sm text-gray-700 dark:text-gray-200">*/}
+        {/*    Software Engineer*/}
+        {/*  </span>*/}
+        {/*</div>*/}
+        <h1>Filters</h1>
+        <div className="flex justify-center">
+          <div>
+            <div className="form-check">
+              <input
+                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label
+                className="form-check-label inline-block text-gray-800"
+                htmlFor="flexCheckDefault"
+              >
+                Default checkbox
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                type="checkbox"
+                value=""
+                id="flexCheckChecked"
+                checked
+              />
+              <label
+                className="form-check-label inline-block text-gray-800"
+                htmlFor="flexCheckChecked"
+              >
+                Checked checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {posts.map((post, index) => (
         <div
           className="max-w-2xl mt-5 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800"
