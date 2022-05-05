a99fdd8bc7edda56e484f0df56202129359417a6
Lane Katris <lanekatris@gmail.com>
2022-05-05 14:37:31 -0400

    undrafted "building my website" article

--- software/js/apps/lkat/pages/article/[slug].tsx
+++ software/js/apps/lkat/pages/article/[slug].tsx
@@ -3,11 +3,16 @@ import { MDXRemote } from 'next-mdx-remote';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import SyntaxHighlighter from 'react-syntax-highlighter';
+// import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import {a11yLight}  from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import { docco as codeColor } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import Image from 'next/image';
 import ScoreCardImage from '../../components/disc-golf/score-card-image/score-card-image';
 import ScoreBadges from '../../components/disc-golf/score-badges/score-badges';
 import Alert from '../../components/alert/alert';
+
+import './index.module.css'
 
 export const getStaticPaths = async () => {
   const files = fs.readdirSync(path.join(process.cwd(), 'posts'));
@@ -38,17 +43,44 @@ export const getStaticProps = async ({ params: { slug } }) => {
   };
 };
 
+const Quote = ({children, author}) => (
+  <blockquote className="relative p-4 text-xl italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500 quote mb-5">
+    {/*<div className="stylistic-quote-mark" aria-hidden="true">*/}
+    {/*  &ldquo;*/}
+    {/*</div>*/}
+    <p className="mb-4">{children}</p>
+    {author && <cite className="flex items-center">
+
+      <div className="flex flex-col items-start">
+        <span className="mb-1 text-sm italic font-bold">- {author}</span>
+      </div>
+    </cite>}
+  </blockquote>
+)
+
+function CodeHighlighter({language = 'javascript', children}) {
+  return <SyntaxHighlighter PreTag={props =><pre className="mb-5" {...props}>{props.children}</pre>} language={language} style={codeColor}>
+    {children}
+  </SyntaxHighlighter>
+}
+
 const components = {
-  SyntaxHighlighter,
+  SyntaxHighlighter: CodeHighlighter,
+  Quote: Quote,
   ScoreCardImage: ScoreCardImage,
   Alert,
   h3: (props) => (
-    <h3 className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
+    <h3 className="text-lg font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
       {props.children}
     </h3>
   ),
+  h2: (props) => (
+    <h2 className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
+      {props.children}
+    </h2>
+  ),
   h1: (props) => (
-    <h1 className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
+    <h1 className="text-2xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">
       {props.children}
     </h1>
   ),
@@ -80,10 +112,11 @@ const components = {
     </>
   ),
   ul: (props) => (
-    <nav className="mt-5 mb-5 flex flex-col sm:items-start sm:text-left text-center items-center space-y-2.5">
+    <nav className="mt-5 mb-5 flex flex-col space-y-2.5">
       {props.children}
     </nav>
   ),
+  blockquote: (props) => <Quote>{props.children}</Quote>
 };
 
 /* eslint-disable-next-line */
