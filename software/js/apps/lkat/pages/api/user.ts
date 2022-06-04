63665ddfda4089811ca3d5868bf6019eeb4cb5ec
Lane Katris <lanekatris@gmail.com>
2022-06-04 14:58:40 -0400

    added hugo and fixed TS errors

--- software/js/apps/lkat/pages/api/user.ts
+++ software/js/apps/lkat/pages/api/user.ts
@@ -1,7 +1,4 @@
-import { masterChiefClient } from '@js/shared';
-
 export default async function handler(req, res) {
-  const idk = await masterChiefClient.idk();
 
-  res.status(200).json(idk);
+  res.status(200).json('hi');
 }
