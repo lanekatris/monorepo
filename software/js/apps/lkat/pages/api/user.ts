2823e49e68bdbc69ea226f69d8eaf58ba20abf6a
Lane Katris <lanekatris@gmail.com>
2022-06-06 07:59:51 -0400

    trying to fix code gen consumed by nextjs

--- software/js/apps/lkat/pages/api/user.ts
+++ software/js/apps/lkat/pages/api/user.ts
@@ -1,4 +1,13 @@
-export default async function handler(req, res) {
+// import { masterChiefClient } from '@js/shared';
 
-  res.status(200).json('hi');
+export default async function handler(req, res) {
+  // const movies = await masterChiefClient.moviesQueryMan();
+  res.status(200).json('fix me');
 }
+
+//
+// export default async function handler(req, res) {
+//   const idk = await masterChiefClient.idk();
+//
+//   res.status(200).json(idk);
+// }
--- software/js/libs/shared/src/lib/masterchief-client.ts
+++ software/js/libs/shared/src/lib/masterchief-client.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
-
+import { getSdk } from './masterchief-sdk';
 
 const client = new GraphQLClient(
   process.env.MASTERCHIEF_URL || 'http://localhost:3333/graphql'
 );
-// export const masterChiefClient = getSdk(client);
+export const masterChiefClient = getSdk(client);
