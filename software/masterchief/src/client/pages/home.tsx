d3ed612946a74d9ed6099ba7b490814f5d55300a
Lane Katris <lanekatris@gmail.com>
2022-06-25 13:28:42 -0400

    zcbcbv

---
+++ software/masterchief/src/client/pages/home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <h1>Hello from NextJS! - Homeeeeeeee and again with hot reload (eyes rollback into head)</h1>;
+};
+
+export default Home;
