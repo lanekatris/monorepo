5027a915e59c544d6bd3b2a0199f0df367e8f536
Lane Katris <lanekatris@gmail.com>
2022-06-25 10:21:39 -0400

    temporary stuff

--- software/js/apps/lkat/pages/api/user.ts
+++ software/js/apps/lkat/pages/api/user.ts
@@ -1,5 +1,10 @@
 // import { masterChiefClient } from '@js/shared';
 
+// The architecture of getting data has to be self hosted with nestjs or whatever
+// because we have to VPC pair with esdb hosting
+
+// so unfortunately we need two sites, or we run nextjs from wherever...
+
 export default async function handler(req, res) {
   // const movies = await masterChiefClient.moviesQueryMan();
   res.status(200).json('fix me');
