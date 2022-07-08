bb17050229edac4c688c3b8c0fe4699907b708e3
Lane Katris <lanekatris@gmail.com>
2022-07-08 18:02:16 -0400

    cleanup failed dependency

--- software/masterchief/src/auth/auth.module.ts
+++ software/masterchief/src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { GuardMe } from './guard-me.guard';
 import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { UserValidator } from './user-validator.service';
 
 @Module({
-  imports: [PassportModule, ConfigModule],
+  imports: [ConfigModule],
   providers: [UserValidator, GuardMe],
   exports: [UserValidator, GuardMe],
   controllers: [AuthController],
--- software/masterchief/src/main.ts
+++ software/masterchief/src/main.ts
@@ -4,7 +4,6 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import * as hbs from 'express-handlebars';
 import { engine } from 'express-handlebars';
 import { HandleUnauthorizedFilter } from './auth/handle-unauthorized.filter';
 async function bootstrap() {
