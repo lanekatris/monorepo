9cb8627d47982d00d09e309edd5fa213d6d20dcf
Lane Katris <lanekatris@gmail.com>
2022-10-21 07:40:40 -0400

    fix for keys

--- software/old-masterchief/frontend/src/pages/dump.tsx
+++ software/old-masterchief/frontend/src/pages/dump.tsx
@@ -40,8 +40,7 @@ export function DumpPage() {
             document: mutation,
             variables: { body },
             requestHeaders: {
-              Authorization:
-                'bearer 0d82fe7998e783339e8397da6a2e10dd0a620d245ecd3fdc7974df7981f2bdc06da1059e777ae856596b4158323c77c778de4993403e1673ce69a5f1d8dd89fb690b43a13bfb4125023fa18108c726da139ad51841e3670d77730ccdb04291c6bec685269b50f3b0b90f56565366a062754fe97024e33473bee2cb9555a1fd7b',
+              Authorization: '',
             },
           });
           console.log('data', data);
