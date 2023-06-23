8754c480ac5c623583314670e4d9878080cb6d68
Lane Katris <lanekatris@gmail.com>
2023-06-23 16:25:11 -0400

    since ubuntu gui to alter monitors doesn't work added a script, also prettier for pulumi aqua

---
+++ scripts/enable-monitors.sh
@@ -0,0 +1,8 @@
+# xrandr --listmonitors
+# Monitors: 3
+# 0: +*HDMI-1 3440/800x1440/335+0+0  HDMI-1
+# 1: +HDMI-0 3440/800x1440/335+3440+0  HDMI-0
+# 2: +HDMI-1-1 1920/521x1080/293+6880+0  HDMI-1-1
+xrandr --output HDMI-1 --auto --primary
+xrandr --output HDMI-0 --auto --right-of HDMI-1
+xrandr --output HDMI-1-1 --auto --right-of HDMI-0
--- software/embedded/aquarium/cloud/index.ts
+++ software/embedded/aquarium/cloud/index.ts
@@ -1,36 +1,36 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import {aquaHandler} from './aqua-handler'
-import { aquaAlerter } from './aqua-alerter'
+import {aquaAlerter} from './aqua-alerter'
 
 const config = new pulumi.Config();
 
 const policy = new aws.iot.Policy("aqua-policy", {
-  policy: JSON.stringify({
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Action: ["iot:*"],
-        Effect: "Allow",
-        Resource: "*",
-      },
-    ],
-  }),
+    policy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Action: ["iot:*"],
+                Effect: "Allow",
+                Resource: "*",
+            },
+        ],
+    }),
 });
 
 const thing = new aws.iot.Thing("aqua-thing");
 export const thingName = thing.name
 
-const cert = new aws.iot.Certificate("aqua-cert", { active: true });
+const cert = new aws.iot.Certificate("aqua-cert", {active: true});
 
 const thingAttachment = new aws.iot.ThingPrincipalAttachment("aqua-principal", {
-  principal: cert.arn,
-  thing: thing.name,
+    principal: cert.arn,
+    thing: thing.name,
 });
 
 const certAttachment = new aws.iot.PolicyAttachment("aqua-cert-principal", {
-  policy: policy.name,
-  target: cert.arn,
+    policy: policy.name,
+    target: cert.arn,
 });
 
 export const privateKey = cert.privateKey;
@@ -39,50 +39,50 @@ export const endpoint = aws.iot.getEndpointOutput();
 export const certificatePem = cert.certificatePem;
 
 const postgresVariables = {
-  PGUSER: config.requireSecret('pgUser'),
-  PGHOST: config.requireSecret('pgHost'),
-  PGPASSWORD: config.requireSecret('pgPassword'),
-  PGDATABASE: config.requireSecret('pgDatabase'),
-  PGPORT: config.requireSecret('pgPort')
+    PGUSER: config.requireSecret('pgUser'),
+    PGHOST: config.requireSecret('pgHost'),
+    PGPASSWORD: config.requireSecret('pgPassword'),
+    PGDATABASE: config.requireSecret('pgDatabase'),
+    PGPORT: config.requireSecret('pgPort')
 }
 
-const aquaLambda = new aws.lambda.CallbackFunction('aqua-listener',{
-  policies: [
-    aws.iam.ManagedPolicies.CloudWatchLogsFullAccess,
-    aws.iam.ManagedPolicies.AWSIoTFullAccess
-  ],
-  callback: aquaHandler,
-  environment:{
-    variables: postgresVariables
+const aquaLambda = new aws.lambda.CallbackFunction('aqua-listener', {
+    policies: [
+        aws.iam.ManagedPolicies.CloudWatchLogsFullAccess,
+        aws.iam.ManagedPolicies.AWSIoTFullAccess
+    ],
+    callback: aquaHandler,
+    environment: {
+        variables: postgresVariables
 
-  }
+    }
 })
 
 const rule = new aws.iot.TopicRule('aquarule', {
-  description: 'Listen for sensor data and persist',
-  enabled : true,
-  sql: thing.name.apply(name => `SELECT * FROM '$aws/things/${name}/testies'`),
-  sqlVersion: '2016-03-23',
-  lambda: {
-    functionArn: aquaLambda.arn
-  }
+    description: 'Listen for sensor data and persist',
+    enabled: true,
+    sql: thing.name.apply(name => `SELECT * FROM '$aws/things/${name}/testies'`),
+    sqlVersion: '2016-03-23',
+    lambda: {
+        functionArn: aquaLambda.arn
+    }
 })
 
 const lambdaPermission = new aws.lambda.Permission('aqua-iot-permission', {
-  action: 'lambda:InvokeFunction',
-  'function': aquaLambda.arn,
-  principal: 'iot.amazonaws.com',
-  sourceArn: rule.arn
+    action: 'lambda:InvokeFunction',
+    'function': aquaLambda.arn,
+    principal: 'iot.amazonaws.com',
+    sourceArn: rule.arn
 })
 
-const aquaAlerterLambda = new aws.lambda.CallbackFunction('aqua-water-level-alerter',{
-  policies: [
-      aws.iam.ManagedPolicies.AmazonSNSFullAccess
-  ],
-  callback: aquaAlerter,
-  environment: {
-    variables: postgresVariables
-  }
+const aquaAlerterLambda = new aws.lambda.CallbackFunction('aqua-water-level-alerter', {
+    policies: [
+        aws.iam.ManagedPolicies.AmazonSNSFullAccess
+    ],
+    callback: aquaAlerter,
+    environment: {
+        variables: postgresVariables
+    }
 })
 
 aws.cloudwatch.onSchedule('aquaWaterLevelLowAlerter', "cron(0 5 * * ? *)", aquaAlerterLambda)
