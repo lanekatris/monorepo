5b1d8f4b931f0669a9947bbb836611a66e8ea3c4
Lane Katris <lanekatris@gmail.com>
2023-06-22 22:44:48 -0400

    created simple aquarium water level alerter

--- software/embedded/aquarium/cloud/aqua-alerter.ts
+++ software/embedded/aquarium/cloud/aqua-alerter.ts
@@ -1,20 +1,33 @@
 const { Client } = require('pg')
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns"
 
-// How do I use .env?
-export async function aquaAlerter(){
+export async function checkWaterLevel() : Promise<boolean> {
     const client = new Client({
         ssl:true
     });
     await client.connect();
-    
+
     const sql = `select * from metric where created_timestamp::date = now()::date and sensor_type = 'float' and value = 0 order by id desc limit 1`;
     const {rows} = await client.query(sql)
     await client.end()
-    console.log('res', rows)
-    
-    if (rows.length > 0) {
-        // alert somebody
+
+    return rows.length > 0;
+}
+
+export async function aquaAlerter() : Promise<void>{
+    const isWaterLevelLow = await checkWaterLevel();
+    if (!isWaterLevelLow) {
+        console.log('Water level looks good, not doing anything')
+        return
     }
-    
-    return true;
+
+    const client = new SNSClient({})
+    const message = `Aquarium needs filled`
+    const command = new PublishCommand({
+        Message: message,
+        Subject: message,
+        TopicArn: 'arn:aws:sns:us-east-1:235680268517:NotifyMe'
+    })
+    const response = await client.send(command);
+    console.log('response', response)
 }
