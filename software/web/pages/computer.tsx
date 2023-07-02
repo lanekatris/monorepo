7b07d53833cc4a4d59496ccbbb7e1f01a58616f6
Lane Katris <lanekatris@gmail.com>
2023-07-02 13:59:50 -0400

    windows support

--- software/web/pages/computer.tsx
+++ software/web/pages/computer.tsx
@@ -1,51 +1,81 @@
-import {GetServerSideProps, InferGetServerSidePropsType} from "next";
-import {useRef} from "react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { useRef } from "react";
 
 interface ComputerInfo {
-    fileCounts: {
-        obsidianVaultRoot: number
-        videosToProcess: number
+  fileCounts: {
+    obsidianVaultRoot: number;
+    videosToProcess: number;
+  };
+}
+
+const headers = {
+  "CF-Access-Client-Id": process.env.CLOUDFLARE_CLIENT_ID!,
+  "CF-Access-Client-Secret": process.env.CLOUDFLARE_CLIENT_SECRET!,
+};
+
+export const getServerSideProps: GetServerSideProps<{
+  info?: ComputerInfo;
+}> = async () => {
+  console.log("headers", headers);
+  const [first, second] = await Promise.all([
+    fetch("https://linux.loonison.com/ping", {
+      headers,
+    }),
+    fetch("https://pc.loonison.com/ping", { headers }),
+  ]);
+
+  // const info = first.status === 200 ? first.json()
+  console.log("statuses", first.status, second.status);
+  let info: ComputerInfo | undefined = undefined;
+  if (first.status === 200) {
+    info = await first.json();
+  }
+  if (second.status === 200) {
+    info = await second.json();
+  }
+
+  console.log("info is", info);
+  // const info = await res.json();
+  return {
+    props: {
+      info,
     },
+  };
+};
 
-}
+export default function ComputerInfoPage({
+  info,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(info);
+  const ref = useRef<HTMLInputElement>(null);
 
-export const getServerSideProps: GetServerSideProps<{info: ComputerInfo}> = async () => {
-    const res = await fetch('https://linux.loonison.com/ping', {
-        headers: {
-            'CF-Access-Client-Id': process.env.CLOUDFLARE_CLIENT_ID!,
-            'CF-Access-Client-Secret': process.env.CLOUDFLARE_CLIENT_SECRET!
-        }
-    })
-    const info = await res.json();
-    return {
-        props: {
-            info
-        }
-    }
-}
+  if (!info) return <h1>no info</h1>;
+  return (
+    <>
+      <h1>computer info</h1>
+      <ul>
+        <li>
+          <b>Obsidian Root Files</b>: {info.fileCounts.obsidianVaultRoot}
+        </li>
+        <li>
+          <b>Videos To Process</b>: {info.fileCounts.videosToProcess}
+        </li>
+      </ul>
 
-export default function ComputerInfoPage({info}: InferGetServerSidePropsType<typeof getServerSideProps>){
-    console.log(info)
-    const ref = useRef<HTMLInputElement>(null);
-    return <>
-        <h1>computer info</h1>
-        <ul>
-            <li><b>Obsidian Root Files</b>: {info.fileCounts.obsidianVaultRoot}</li>
-            <li><b>Videos To Process</b>: {info.fileCounts.videosToProcess}</li>
-        </ul>
-
-        <h1>Go to sleep</h1>
-        <form onSubmit={async e => {
-            e.preventDefault();
-            console.log('submit', ref.current!.value)
-            await fetch('/api/sleep', {
-                method: 'POST',
-                body: JSON.stringify({password: ref.current!.value})
-            })
-        }}>
-            <input placeholder="password" type="password" ref={ref}/>
-            <button type="submit">Go to sleep</button>
-        </form>
-
-        </>
+      <h1>Go to sleep</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log("submit", ref.current!.value);
+          await fetch("/api/sleep", {
+            method: "POST",
+            body: JSON.stringify({ password: ref.current!.value }),
+          });
+        }}
+      >
+        <input placeholder="password" type="password" ref={ref} />
+        <button type="submit">Go to sleep</button>
+      </form>
+    </>
+  );
 }
