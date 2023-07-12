063c9dba08955377b4c2ca9433e9afd5001e4622
Lane Katris <lanekatris@gmail.com>
2023-07-11 21:33:48 -0400

    support read later

--- software/chrome-extension/popup.js
+++ software/chrome-extension/popup.js
@@ -1,12 +1,3 @@
-const apiKeyEl = document.getElementById("api-key");
-
-apiKeyEl.addEventListener("keyup", (e) => {
-  localStorage.setItem("apikey", e.target.value);
-  console.log("setting", e.target.value);
-});
-
-apiKeyEl.value = localStorage.getItem("apikey");
-
 const readLaterButtonEl = document.getElementById("read-later-button");
 
 readLaterButtonEl.addEventListener("click", () => {
@@ -21,34 +12,13 @@ readLaterButtonEl.addEventListener("click", () => {
       console.log(activeTab);
 
       readLaterButtonEl.textContent = "Saving...";
-      fetch("http://localhost:5298/graphql/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          // 'x-api-key':
-        },
-        // mode: "no-cors",
-        body: JSON.stringify({
-          query: `mutation CreateBookmark($url: String!, $tags: [String!]) {
-        bookmarkCreate(url: $url, tags: $tags) {
-          id
-          name
-        }
-      }`,
-          variables: {
-            url: activeTab.url,
-            tags: ["read-later"],
-          },
-        }),
-      }).then((response) => {
-        console.log("graphql response", response);
-        if (response.ok) {
-          readLaterButtonEl.textContent = "Success!";
-        } else {
-          readLaterButtonEl.textContent = "Failed";
-        }
-      });
+      fetch(`http://localhost:8080/inbox/submit?url=${activeTab.url}`)
+          .then((response) => {
+              readLaterButtonEl.textContent = "Saved!";
+          })
+          .catch(e => {
+              readLaterButtonEl.textContent = "Error";
+          });
     }
   );
 });
