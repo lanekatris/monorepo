4c39455e69efae60e4dc2b89913d8fa9af0df305
Lane Katris <lanekatris@gmail.com>
2023-07-20 06:36:47 -0400

    renamed scorecards project and added deploy script for the go binaries

--- software/js/packages/scorecards/jest.config.ts
+++ software/js/packages/scorecards/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'ingest-udisc-scorecards',
+  displayName: 'scorecards',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/ingest-udisc-scorecards',
+  coverageDirectory: '../../coverage/packages/scorecards',
 };
