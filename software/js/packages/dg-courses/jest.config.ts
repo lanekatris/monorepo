4155bc2aeb1f5217b9ea7e9b639d5906e2c5772c
Lane Katris <lanekatris@gmail.com>
2023-07-28 15:33:17 -0400

    added initiatl project for the rewrite of creating a dg courses dataset

---
+++ software/js/packages/dg-courses/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'dg-courses',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/dg-courses',
+};
