48e4aa41be3786c9f1906e5a6ea4507a9d420ec9
Lane Katris <lanekatris@gmail.com>
2023-09-10 13:45:38 -0400

    little bit of landing page for book logger

--- software/js/packages/book-logger/app/page.tsx
+++ software/js/packages/book-logger/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Index() {
   return (
     <div className="container mx-auto">
-      <div className="bg-indigo-500-p-2 font-mono">Hello!</div>
+      {/*<div className="bg-indigo-500-p-2 font-mono">Hello!</div>*/}
 
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
@@ -12,7 +12,7 @@ export default async function Index() {
           />
           <div className="text-center lg:w-2/3 w-full">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Microdosing synth tattooed vexillologist
+              Log your child's books read and meet your goals!
             </h1>
             <p className="mb-8 leading-relaxed">
               Meggings kinfolk echo park stumptown DIY, kale chips beard
@@ -32,6 +32,13 @@ export default async function Index() {
           </div>
         </div>
       </section>
+      <div
+        className="bg-green-50 border border-green-200 text-sm text-green-600 rounded-md p-4"
+        role="alert"
+      >
+        <span className="font-bold">Success</span> alert! You should check in on
+        some of those fields below.
+      </div>
     </div>
   );
 }
