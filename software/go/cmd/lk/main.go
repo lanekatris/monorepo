9a618eaf3183d86e116c87fb7de6a74010c31af0
Lane Katris <lanekatris@gmail.com>
2023-10-27 19:14:54 -0400

    link fixes, playing with go cobra, starting script to copy brain from obsidian

--- software/go/cmd/lk/main.go
+++ software/go/cmd/lk/main.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
+	"io/fs"
 	pb "lkat/pkg/pb"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 var rootCmd = &cobra.Command{
@@ -32,13 +36,38 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-func Execute() error {
-	//ctx := context.TODO()
-	return rootCmd.Execute()
+var jsonTestCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "Looks at markdown files and creates json as events",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("This is where I ls the dir")
+		err := filepath.Walk(args[0], func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fmt.Println(path, info.Size())
+			return nil
+		})
+		if err != nil {
+			log.Println(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(jsonTestCmd)
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
 }
 
 func main() {
 	//fmt.Println("Im supposed to be the client ha")
 	//ctx := context.Background()
 	Execute()
+	//rootCmd.AddCommand(jsonTestCmd)
 }
