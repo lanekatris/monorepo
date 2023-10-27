371ef3eb24f8dc5357d2dc3036f728b366afd49c
Lane Katris <lanekatris@gmail.com>
2023-10-27 19:25:23 -0400

    fixing brain copy script

---
+++ scripts/sync-brain.ps1
@@ -0,0 +1,8 @@
+Copy-Item -Recurse -Force -Path C:\Users\looni\OneDrive\Documents\vault1\Public\* C:\Code\brain\content
+
+Set-Location C:\Code\brain
+
+git commit -m "Updated brain from Obsidian"
+git push
+
+Set-Location -
