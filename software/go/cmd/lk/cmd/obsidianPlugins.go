8541b0a3d05fe8d4e61ec0b994502ca5df3378f9
Lane Katris <lanekatris@gmail.com>
2023-10-29 22:27:13 -0400

    generating obsidian plugin markdown now

--- software/go/cmd/lk/cmd/obsidianPlugins.go
+++ software/go/cmd/lk/cmd/obsidianPlugins.go
@@ -6,6 +6,9 @@ package cmd
 import (
 	"fmt"
 	"github.com/goccy/go-json"
+	"time"
+
+	"github.com/atsushinee/go-markdown-generator/doc"
 	"github.com/spf13/cobra"
 	"os"
 	"sort"
@@ -16,36 +19,45 @@ var pluginsCmd = &cobra.Command{
 	Use:   "plugins",
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
-		//file, err := os.Open("C:\\Users\\looni\\OneDrive\\Documents\\vault1\\.obsidian\\community-plugins.json")
-		//if err != nil {
-		//	fmt.Println("Error opening file:", err)
-		//	return
-		//}
-		//defer file.Close()
-		//
-		//decoder := json.NewDecoder(file)
-		//
-		//var data []string
-		//
-		//err = decoder.Decode(&data)
-		//if err != nil {
-		//	fmt.Println("Error decoding JSON:", err)
-		//	return
-		//}
 		var data = GetCommunityPlugins()
 		var quickAddChoices = GetQuickAddConfiguration()
 
-		fmt.Println("Community Plugins:")
+		//fmt.Println("Community Plugins:")
+		//for i := 0; i < len(data); i++ {
+		//	fmt.Println(data[i])
+		//}
+
+		//fmt.Println()
+		//fmt.Println()
+		//fmt.Println("Quick Add Choices:")
+		//for i := 0; i < len(quickAddChoices); i++ {
+		//	fmt.Println(quickAddChoices[i])
+		//}
+
+		// TODO: Toggle markdown or stdout based on format flag?
+		idk := doc.NewMarkDown()
+
+		idk.Write("**Generated On**: " + time.Now().Format("2006-01-02 15:04:05"))
+		idk.WriteLines(2)
+
+		idk.WriteLevel1Title("Community Plugins")
+		idk.Writeln()
 		for i := 0; i < len(data); i++ {
-			fmt.Println(data[i])
+			idk.Write("- " + data[i])
+			idk.Writeln()
 		}
 
-		fmt.Println()
-		fmt.Println()
-		fmt.Println("Quick Add Choices:")
+		idk.WriteLines(2)
+		idk.WriteLevel1Title("Quick Add Choices")
+		idk.Writeln()
 		for i := 0; i < len(quickAddChoices); i++ {
-			fmt.Println(quickAddChoices[i])
+			idk.Write("- " + quickAddChoices[i])
+			idk.Writeln()
 		}
+
+		idk.Export("C:\\Users\\looni\\OneDrive\\Documents\\vault1\\Public\\My Obsidian Plugins (Generated).md")
+
+		fmt.Println(idk.String())
 	},
 }
 
--- software/go/go.mod
+++ software/go/go.mod
@@ -3,8 +3,13 @@ module lkat
 go 1.18
 
 require (
+	github.com/atsushinee/go-markdown-generator v0.0.0-20231027094725-92d26ffbe778
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
+	github.com/goccy/go-json v0.10.0
+	github.com/spf13/cobra v1.7.0
+	google.golang.org/grpc v1.56.2
+	google.golang.org/protobuf v1.30.0
 )
 
 require (
@@ -14,7 +19,6 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
-	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -24,7 +28,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
@@ -34,7 +37,5 @@ require (
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
-	google.golang.org/grpc v1.56.2 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
