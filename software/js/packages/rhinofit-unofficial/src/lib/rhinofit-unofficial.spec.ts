bcf5cdd507d7ab700a7961622e3a6e239eb9bd0b
Lane Katris <lanekatris@gmail.com>
2024-02-24 13:30:28 -0500

    rhino fit tests wip

--- software/js/packages/rhinofit-unofficial/src/lib/rhinofit-unofficial.spec.ts
+++ software/js/packages/rhinofit-unofficial/src/lib/rhinofit-unofficial.spec.ts
@@ -4,6 +4,7 @@ const data: MembersResponse = require('./access-tracking.json');
 const reservations: ReservationsResponse = require('./reservations.json');
 import cheerio from 'cheerio';
 import { isSameDay } from 'date-fns';
+const timeslots: Timeslot[] = require('./timeslots.json');
 
 describe('rhinofitUnofficial', () => {
   it('should work', () => {
@@ -58,14 +59,14 @@ describe('rhinofitUnofficial', () => {
     // todo: log into calendar
 
     // so we know our dac entries, but what about reservations?
-    // const hackReservationNow = new Date(2024, 1, 17, 18, 34);
-    // const reservationsUserIds = new Set(
-    //   reservations.aaData
-    //     .filter((x) => reservationIsToday(x, hackReservationNow))
-    //     .map(findReservtionUserId)
-    // );
+    const hackReservationNow = new Date(2024, 1, 17, 18, 34);
+    const reservationsUserIds = new Set(
+      reservations.aaData
+        .filter((x) => reservationIsToday(x, hackReservationNow))
+        .map(findReservtionUserId)
+    );
     //
-    // console.log('reservations', reservationsUserIds);
+    console.log('reservations user ids', reservationsUserIds);
 
     // Is there a reservation for their dac entry?
     // We hve a time constraint here, not just user id
@@ -75,9 +76,23 @@ describe('rhinofitUnofficial', () => {
       // if there are multiple dacs we don't need to make multiple reservations
       // find the closest reservation key to their dac
       // we need to know if this user has a reservation
+      const dacUserId = findUserId(dac);
+
+      if (reservationsUserIds.has(dacUserId)) {
+        console.log('user has reservation');
+      } else {
+        stuffToDo.push(`Need to reserve for user id ${dacUserId}`);
+      }
     }
 
     // Get public feed of available reservation... we only need to do this if we are missing a reservation for someone...
+
+    console.log('stuff to do', stuffToDo);
+
+    // now we have to figure out how to make a rservation, we alrady figured out kiosk
+
+    // todo: if we need to rserve load the public feed
+    // todo: we need to know the timeslot of their first dac
   });
 
   describe('date tests', () => {
@@ -92,7 +107,30 @@ describe('rhinofitUnofficial', () => {
       expect(findReservtionUserId(reservations.aaData[0])).toEqual('846282');
     });
   });
+
+  describe('findTimeslotFromDac', () => {
+    it('should find like normal', () => {
+      const dac = data.aaData.find((x) => x.DT_RowId == 'al_28655515');
+      expect(dac).not.toBeNull();
+
+      const timeslot = findTimeslotFromDac(dac!);
+      expect(timeslot).not.toBeNull();
+      console.log('timeslot! for ' + dac?.al_cal_datetime, timeslot);
+    });
+  });
 });
+
+function findTimeslotFromDac(member: Member) {
+  const date = new Date(member.al_cal_datetime);
+
+  // find the closest timeslot.. but what about if you are on the hour, or what if you almost at the end of the 2 hours?
+  const timeslot = timeslots.find((x) => {
+    const start = new Date(x.start);
+    const end = new Date(x.end);
+    return date >= start && date <= end;
+  });
+  return timeslot;
+}
 
 function onlyToday({ al_cal_datetime }: Member, now: Date) {
   const date = new Date(al_cal_datetime);
@@ -149,4 +187,16 @@ export interface Reservation {
   c_name: string;
   r_key: string;
   r_timestamp: Date;
+}
+
+export interface Timeslot {
+  start: string;
+  end: string;
+  allDay: boolean;
+  title: string;
+  color: string;
+  origcolor: string;
+  reservation: number;
+  id: string;
+  day: string;
 }
