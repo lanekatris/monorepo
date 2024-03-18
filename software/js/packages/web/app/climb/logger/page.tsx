7f5036cf5ac37dd583180dd224cbed8124885194
Lane Katris <lanekatris@gmail.com>
2024-03-18 16:25:04 -0400

    trying to build with admin views

--- software/js/packages/web/app/climb/logger/page.tsx
+++ software/js/packages/web/app/climb/logger/page.tsx
@@ -6,24 +6,24 @@ interface ClimbListLog {
   Date: Date;
 }
 
-async function getClimbLogs() {
-  const client = new Client({
-    ssl: true,
-    connectionString: process.env.POSTGRES_CONN_URL,
-  });
-
-  await client.connect();
-  const sql = `select "Date" from noco.climb_log_v1 group by "Date" order by "Date" desc`;
-
-  const { rows }: { rows: ClimbListLog[] } = await client.query(sql);
-  await client.end();
-
-  return rows;
-}
-
+// async function getClimbLogs() {
+//   const client = new Client({
+//     ssl: true,
+//     connectionString: process.env.POSTGRES_CONN_URL,
+//   });
+//
+//   await client.connect();
+//   const sql = `select "Date" from noco.climb_log_v1 group by "Date" order by "Date" desc`;
+//
+//   const { rows }: { rows: ClimbListLog[] } = await client.query(sql);
+//   await client.end();
+//
+//   return rows;
+// }
+//
 export default async function Page() {
   if (!isAdmin()) return <Alert color="danger">Not Authorized</Alert>;
-  const climbLogs = await getClimbLogs();
+  // const climbLogs = await getClimbLogs();
   // console.log(climbLogs);
   return (
     <main className="mx-5 mt-5">
