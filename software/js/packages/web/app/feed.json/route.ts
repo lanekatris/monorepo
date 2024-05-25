fe08215beead2b5495bc3c84bb20360d28600d4e
Lane Katris <lanekatris@gmail.com>
2024-05-25 08:44:53 -0400

    cache fix

--- software/js/packages/web/app/feed.json/route.ts
+++ software/js/packages/web/app/feed.json/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { getFeed } from '../../feed/get-feed';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 export async function GET() {
   const data = await getFeed();
   return NextResponse.json(data, { status: 200 });
