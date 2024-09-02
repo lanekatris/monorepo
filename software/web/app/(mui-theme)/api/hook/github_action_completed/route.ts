da7540480a6c7ed1fb2f0022f7464f11598cb992
Lane Katris <lanekatris@gmail.com>
2024-09-01 20:47:24 -0400

    cleanup and testing if watchtower works

--- software/web/app/(mui-theme)/api/hook/github_action_completed/route.ts
+++ software/web/app/(mui-theme)/api/hook/github_action_completed/route.ts
@@ -8,20 +8,14 @@ export async function GET(request: NextRequest) {
   if (token !== process.env.WEB_API_TOKEN)
     return NextResponse.json({ error: 'Bad token' }, { status: 401 });
 
-  // console.log('restarting web in 1 second');
-  // setTimeout(async () => {
-  const { stdout, stderr } = await exec(
-    // `docker pull loonison101/web && docker restart web`
-    `docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once web`
-  );
-  console.log({ stdout, stderr });
-  // }, 1000);
+  console.log('restarting web in 1 second...');
+  setTimeout(async () => {
+    console.log('invoking docker...');
+    const { stdout, stderr } = await exec(
+      `docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once web`
+    );
+    console.log({ stdout, stderr });
+  }, 1000);
 
-  // try {
-  //   const { stdout, stderr } = await exec(`docker restart web`);
-  //   return NextResponse.json({ stdout, stderr }, { status: 200 });
-  // } catch (e) {
-  //   return NextResponse.json({ error: e }, { status: 500 });
-  // }
   return NextResponse.json({ status: 200 });
 }
