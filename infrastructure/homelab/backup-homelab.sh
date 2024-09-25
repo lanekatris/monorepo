f167eae5bd9f38bdc9c95ab34501c2b8072b0cd4
Lane Katris <lanekatris@gmail.com>
2024-09-25 16:06:19 -0400

    update backups for immich and miniflux, make n8n UI work after upgrade, support restarting app by mounting docker.sock

--- infrastructure/homelab/backup-homelab.sh
+++ infrastructure/homelab/backup-homelab.sh
@@ -1,4 +1,9 @@
 restic backup /bigboy/temp
+
+restic backup /bigboy/immich/library
 restic backup /bigboy/immich/upload
+restic backup /bigboy/immich/profile
 restic backup /bigboy/immich/db_dumps
+
 restic backup /bigboy/minio
+restic backup /bigboy/miniflux
