93a059175ae849e3a44437caeec3b6aac14c6438
Lane Katris <lanekatris@gmail.com>
2024-10-20 21:57:51 -0400

    simplified backup script, its not used but still here
    always start miniflux
    
    always start temporal

--- infrastructure/homelab/backup-homelab.sh
+++ infrastructure/homelab/backup-homelab.sh
@@ -1,9 +1,10 @@
 restic backup /bigboy/temp
 
-restic backup /bigboy/immich/library
-restic backup /bigboy/immich/upload
-restic backup /bigboy/immich/profile
-restic backup /bigboy/immich/db_dumps
+#restic backup /bigboy/immich/library
+#restic backup /bigboy/immich/upload
+#restic backup /bigboy/immich/profile
+#restic backup /bigboy/immich/db_dumps
+restic backup /bigboy/immich
 
 restic backup /bigboy/minio
 restic backup /bigboy/miniflux
