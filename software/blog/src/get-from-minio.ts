6dd966ec9bb9beb2fc9f92a4d6249f1371e653fc
Lane Katris <lanekatris@gmail.com>
2024-11-04 13:00:37 -0500

    added homelab to astro

---
+++ software/blog/src/get-from-minio.ts
@@ -0,0 +1,47 @@
+import * as Minio from "minio";
+import type { BucketItemStat } from "minio";
+
+const minioClient = new Minio.Client({
+  endPoint: "192.168.86.100", // e.g., 'play.min.io'
+  port: 9000, // e.g., 9000
+  useSSL: false, // Set to false if not using SSL
+  accessKey: import.meta.env.MINIO_A, // process.env.MINIO_A!,
+  secretKey: import.meta.env.MINIO_S,
+});
+
+export async function getFromMinio<T>(
+  bucketName: string,
+  objectName: string,
+  isJson: boolean = true,
+): Promise<{
+  data: T;
+  stats: BucketItemStat;
+}> {
+  try {
+    const stream = await minioClient.getObject(bucketName, objectName);
+    const stats = await minioClient.statObject(bucketName, objectName);
+
+    let data = "";
+    stream.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    return new Promise((resolve, reject) => {
+      stream.on("end", () => {
+        try {
+          resolve(isJson ? { data: JSON.parse(data), stats } : { data, stats });
+        } catch (error) {
+          reject(new Error("Error parsing JSON"));
+        }
+      });
+
+      stream.on("error", (error) => {
+        reject(error);
+      });
+    });
+  } catch (error) {
+    // @ts-ignore
+    console.error(`Error retrieving object: ${error.message}`);
+    throw error;
+  }
+}
