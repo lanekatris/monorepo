f97c77a8917dccf460fe8916b9ec24887a696469
Lane Katris <lanekatris@gmail.com>
2025-01-02 11:32:25 -0500

    charts are now svg

--- software/blog/src/renderGraph.ts
+++ software/blog/src/renderGraph.ts
@@ -1,7 +1,8 @@
 import type { ChartConfiguration } from 'chart.js'
 import { ChartJSNodeCanvas } from 'chartjs-node-canvas'
 
-export async function renderGraph(height: number, width: number, config: ChartConfiguration) {
-	const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height })
-	return chartJSNodeCanvas.renderToDataURL(config)
+export function renderGraph(height: number, width: number, config: ChartConfiguration) {
+	const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, type: 'svg' })
+	const buffer = chartJSNodeCanvas.renderToBufferSync(config, 'image/svg+xml')
+	return buffer
 }
