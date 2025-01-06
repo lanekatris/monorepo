2986a04ba849dcc4e7836bf420ea45cdd8f5b7af
Lane Katris <lanekatris@gmail.com>
2025-01-06 08:05:50 -0500

    notebook additions, last modified from git, TOC fix

--- software/blog/astro.config.mjs
+++ software/blog/astro.config.mjs
@@ -3,15 +3,27 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 // import wikiLinkPlugin from "@portaljs/remark-wiki-link";
 import sitemap from '@astrojs/sitemap'
+import { execSync } from 'child_process'
+
+export function remarkModifiedTime() {
+	// @ts-ignore
+	return function (tree, file) {
+		const filepath = file.history[0]
+		// console.log(filepath)
+		const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`)
+		file.data.astro.frontmatter.lastModified = result.toString()
+	}
+}
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lanekatris.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx({ remarkPlugins: [remarkModifiedTime] }), sitemap()],
 	markdown: {
 		// shikiConfig: {
 		// 	theme: 'dracula'
 		// }
-		syntaxHighlight: false
+		syntaxHighlight: false,
+		remarkPlugins: [remarkModifiedTime]
 	}
 })
