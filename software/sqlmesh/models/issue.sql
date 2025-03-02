1c68989e32367c92e5ef3daaeedb4c89dab73646
Lane Katris <lanekatris@gmail.com>
2025-03-02 10:46:09 -0500

    added check for rhinofit sync to issues

--- software/sqlmesh/models/issue.sql
+++ software/sqlmesh/models/issue.sql
@@ -3,3 +3,8 @@ MODEL (
       );
 
 select concat('Volleyball needs scores updated for: ', opponent) message from noco.volleyball where date < now() and (sets_won is null or sets_lost is null)
+union
+select 'Rhinofit sync may have an issue' message where not exists (select *
+                                                                   from events
+                                                                   where event_name = 'rhinofit_worker_v1'
+                                                                     and created_at > now() - interval '16 minutes')
