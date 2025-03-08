cd014a5e6ab432309d39eac259a174f1923f5af0
Lane Katris <lanekatris@gmail.com>
2025-03-07 19:31:11 -0500

    created raindrop bookmark model

---
+++ software/sqlmesh/models/bookmark.sql
@@ -0,0 +1,14 @@
+MODEL (
+    name models.bookmark
+      );
+
+with x as (
+    select created_at, data::jsonb data from events where event_name = 'bookmark_v1'
+)
+, y as (
+
+select created_at updated, data->> 'link' link, data->> 'title' title, (xx.data->>'collectionId')::int collection_id
+from x xx
+)
+select yy.*, rc.name collection_name from y yy
+left join models.raindrop_collection rc on rc.id = yy.collection_id
