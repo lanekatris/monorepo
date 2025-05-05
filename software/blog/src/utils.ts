4a577901268bc5d5aa0c26894d261a827eb3503f
Lane Katris <lanekatris@gmail.com>
2025-05-05 07:30:18 -0400

    Starting files structure to show on the /data page

--- software/blog/src/utils.ts
+++ software/blog/src/utils.ts
@@ -22,3 +22,48 @@ export function convertToCSV(jsonData) {
 export const FILE_URLS = {
 	PODCAST_JSON: '/podcasts.json'
 }
+
+// todo: load from directory
+// todo: validate the pages as subpages and zod valdiates
+export const FILES = [
+	{
+		type: 'podcast_json',
+		filename: 'podcasts.json',
+		pages: ['/podroll'],
+		notes: 'It is using this Audiobookshelf api: TODO. There is a subarray of the data.'
+	},
+	{
+		type: 'podcast_opml',
+		filename: 'podcasts.opml',
+		pages: ['/podroll']
+	},
+	{
+		type: 'volleyball-csv',
+		filename: '2025-volleyball-data.csv',
+		pages: ['/2025-volleyball']
+	},
+	{
+		type: 'volleyball-ics',
+		filename: '2025-volleyball.ics',
+		pages: ['/2025-volleyball']
+	},
+	{
+		type: 'blogroll-csv',
+		filename: 'blogroll.xml',
+		pages: ['/blogroll']
+	},
+	{
+		type: 'dgpt-winners-json',
+		filename: 'dgpt-winners.json',
+		pages: ['/disc-golf']
+	},
+	{
+		type: 'dgpt-winners-csv',
+		filename: 'dgpt-winners.csv',
+		pages: ['/disc-golf']
+	}
+]
+
+export function getFileDownloadPath(fileName: string) {
+	return `/${fileName}`
+}
