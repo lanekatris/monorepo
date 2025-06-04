d8040c5815c520da88ec30d4b50b26918f1c272d
Lane Katris <lanekatris@gmail.com>
2025-06-04 08:35:00 -0400

    wired neotrellis button press to putting computer to sleep

--- software/go/workflow_dumper.go
+++ software/go/workflow_dumper.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/charmbracelet/log"
+	"github.com/google/uuid"
 	"go.temporal.io/api/enums/v1"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
@@ -31,6 +32,10 @@ func (input *WorkflowInputDumper) DumpEvent(eventName string, data string) error
 type ToggleSwitchedData struct {
 	Value    int    `json:"value"`
 	SwitchId string `json:"switch_id"`
+}
+
+type NeotrellisData struct {
+	ButtonId int `json:"buttonId"`
 }
 
 func (input *WorkflowInputDumper) ProcessEvent(eventName string, data string) error {
@@ -175,6 +180,38 @@ func WorkflowDumper(ctx workflow.Context, eventName string, data string) error {
 	err = workflow.ExecuteActivity(ctx, activities.DumpEvent, eventName, data).Get(ctx, nil)
 	if err != nil {
 		return err
+	}
+
+	if eventName == "neotrellis_v1" {
+		var neotrellisData NeotrellisData
+		var err = json.Unmarshal([]byte(data), &neotrellisData)
+		if err != nil {
+			return err
+		}
+
+		if neotrellisData.ButtonId == 12 {
+			childOptions := workflow.ChildWorkflowOptions{
+				WorkflowID:               "sleep-from-neotrellis-" + uuid.New().String(),
+				TaskQueue:                GreetingTaskQueue,
+				ParentClosePolicy:        enums.PARENT_CLOSE_POLICY_ABANDON,
+				WorkflowExecutionTimeout: time.Minute,
+				RetryPolicy: &temporal.RetryPolicy{
+					MaximumAttempts: 1,
+				},
+			}
+
+			childCtx := workflow.WithChildOptions(ctx, childOptions)
+
+			err = workflow.ExecuteChildWorkflow(childCtx, WorkflowSleep).GetChildWorkflowExecution().Get(childCtx, nil)
+
+			if err != nil {
+				return err
+			}
+
+		} else {
+			log.Info("Not mapped", "event_id", neotrellisData.ButtonId)
+		}
+
 	}
 
 	childOptions := workflow.ChildWorkflowOptions{
