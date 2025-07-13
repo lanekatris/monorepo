4cecfdb399df0e707d904b080618e2290f20fcf2
Lane Katris <lanekatris@gmail.com>
2025-07-13 10:11:44 -0400

    im at a different location, away from my home server, which is away from my elgato light

--- software/web/lib/createEvent.ts
+++ software/web/lib/createEvent.ts
@@ -1,14 +1,20 @@
 import { nanoid } from 'nanoid';
 import { getTemporalClient } from './getTemporalClient';
 
+type TASK_QUEUE = 'server' | 'GREETING_TASK_QUEUE';
+
 export async function createEvent(
   eventName: string,
   data: string | undefined = undefined
 ) {
   const client = await getTemporalClient();
+
+  const taskQueue: TASK_QUEUE =
+    eventName === 'neotrellis_v1' ? 'GREETING_TASK_QUEUE' : 'server';
+
   return await client.workflow.start('WorkflowDumper', {
     workflowId: `nextjs-event-dumper-${eventName}-${nanoid()}`,
-    taskQueue: 'server',
+    taskQueue,
     args: [eventName, data]
   });
 }
