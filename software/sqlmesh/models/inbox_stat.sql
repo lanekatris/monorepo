f18e94d34f528b87f242dd69f8d4d6e1359c27a8
Lane Katris <lanekatris@gmail.com>
2025-07-13 10:11:55 -0400

    not sure why this was here

--- software/sqlmesh/models/inbox_stat.sql
+++ software/sqlmesh/models/inbox_stat.sql
@@ -4,5 +4,5 @@ MODEL (
 
 select (data::jsonb->'unreadCount')::int unread_count from public.events
 where event_name= 'inbox_data_received_v1'
-  and (data::jsonb->'unreadCount')::int > 0
+--   and (data::jsonb->'unreadCount')::int > 0
 order by created_at desc limit 1
