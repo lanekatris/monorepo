e30a1b5a6b62bbf574452c4467c349656d176347
Lane Katris <lanekatris@gmail.com>
2025-08-03 09:05:53 -0400

    1password and citrix working, albeit some manual steps for citrix

--- infrastructure/nixos/configuration.nix
+++ infrastructure/nixos/configuration.nix
@@ -114,6 +114,7 @@ obsidian
 spotify
 discord
 oterm # for ollama/open-qqwebui
+citrix_workspace
 #pkgs.ollama
 #   (pkgs.ollama.override {
 #      acceleration = "cuda";
@@ -122,6 +123,14 @@ oterm # for ollama/open-qqwebui
 #    pkgs.dconf
 #    gnomeExtensions.forge
   ];
+
+   programs._1password.enable = true;
+    programs._1password-gui = {
+      enable = true;
+      # Certain features, including CLI integration and system authentication support,
+      # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+      polkitPolicyOwners = [ "lane" ];
+    };
 
   services.ollama = {
     # package = pkgs.unstable.ollama; # If you want to use the unstable channel package for example
