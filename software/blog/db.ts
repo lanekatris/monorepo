bad2258c64a20c7238e03fec8bf18e0f452beb54
Lane Katris <lanekatris@gmail.com>
2025-10-01 05:43:17 -0400

    starting to get my blog working with vanilla postgres

--- software/blog/db.ts
+++ software/blog/db.ts
@@ -4,9 +4,16 @@
 //
 // export default db;
 
-import pg from 'pg'
+import {Pool} from 'pg'
 
-const client = new pg.Client(import.meta.env.DATABASE_URL)
+// const client = new pg.Client(import.meta.env.DATABASE_URL)
+const pool = new Pool({
+	connectionString: import.meta.env.DATABASE_URL
+})
 
-await client.connect()
-export default client;
+// await client.connect()
+export async function query(text: any, params?: any) {
+	const res = await pool.query(text, params);
+	return res.rows;
+}
+// export default client;
--- software/blog/src/content/blog/Started 2025 Winter and Spring Local Volleyball League.md
+++
@@ -1,4 +0,0 @@
----
-pubDate: 2025-09-12
----
-so we did this
