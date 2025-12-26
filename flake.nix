85b8f1711cdfcdb0a90ebd415d2fb8fb4b7409ad
Lane Katris <lanekatris@gmail.com>
2025-12-26 09:44:12 -0500

    i lost work where this was a flake, just using go for now

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
-# save this as shell.nix
-{ pkgs ? import <nixpkgs> {}}:
+{
+  description = "Development environment";
 
-pkgs.mkShell {
-  packages = [ pkgs.go ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.go ];
+      };
+    };
 }
